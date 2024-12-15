544df22cb26cfdab70c25ff10985add408f1c5e8
bulbajs <93475048+bulbajs@users.noreply.github.com>
2024-12-15 23:32:15 +0300

    +d4.2
    Добавил фильтры

--- d4_module_pw/simpleapp/views.py
+++ d4_module_pw/simpleapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Product
 from datetime import datetime
+from .filters import ProductFilter
 
 
 class ProductList(ListView):
@@ -9,15 +10,20 @@ class ProductList(ListView):
     ordering = 'name'
     template_name = 'products.html'
     context_object_name = 'products'
-    paginate_by = 1
-
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['time_now'] = datetime.utcnow()
         context['next_sale'] = 'Убрал строку None'
+        context['filterset'] = self.filterset
         return context
 
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        return self.filterset.qs
 
 class ProductDetail(DetailView):
     model = Product
